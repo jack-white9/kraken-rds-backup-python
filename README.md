3409f22f05cebd5520e314c80c21f66b95f5c872
Jack White <jfw793@gmail.com>
2024-07-21 22:37:30 +1000

    create basic db backup logic

---
+++ README.md
@@ -0,0 +1,22 @@
+# RDS Backup
+
+## Setup
+
+1. Setup virtual environment. _**Note**: Although Python 3.8 has been used for this example, the script is forward-compatible with versions >= 3.8. Use your preferred version._
+
+```sh
+python3.8 -m venv .venv
+source .venv/bin/activate
+```
+
+2. Install Python dependencies
+
+```sh
+pip install -r requirements.txt
+```
+
+## Usage
+
+```sh
+python rds_backup.py
+```
---
+++ rds_backup.py
@@ -0,0 +1,76 @@
+import boto3
+import botocore
+import logging
+from typing import TypedDict, List
+from datetime import datetime, timezone
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# define custom snapshot type
+class Snapshot(TypedDict):
+    IsSnapshotSuccessful: bool
+    DBClusterSnapshotIdentifier: str
+    DBClusterIdentifier: str
+    DBClusterSnapshotArn: str
+    SnapshotType: str
+    SnapshotCreateTime: str
+    AvailabilityZones: List[str]
+    VpcId: str
+
+
+def create_snapshot(db: str) -> Snapshot:
+    client = boto3.client("rds")
+    utc_timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S")
+    snapshot_id = f"{db}-snapshot-{utc_timestamp}"
+    try:
+        response = client.create_db_cluster_snapshot(
+            DBClusterSnapshotIdentifier=snapshot_id,
+            DBClusterIdentifier=db,
+            Tags=[{"Key": "Database", "Value": db}],
+        )
+        logger.info(f"Snapshot created: {snapshot_id}")
+        data = response["DBClusterSnapshot"]
+        return {
+            "IsSnapshotSuccessful": True,
+            "DBClusterSnapshotIdentifier": snapshot_id,
+            "DBClusterIdentifier": db,
+            "DBClusterSnapshotArn": data["DBClusterSnapshotArn"],
+            "SnapshotType": data["SnapshotType"],
+            "SnapshotCreateTime": data["SnapshotCreateTime"].strftime("%Y%m%d%H%M%S"),
+            "AvailabilityZones": data["AvailabilityZones"],
+            "VpcId": data["VpcId"],
+        }
+    except botocore.exceptions.ClientError as error:
+        logger.error(f"Error creating snapshot for {db}: {error}")
+        return {
+            "IsSnapshotSuccessful": False,
+            "DBClusterSnapshotIdentifier": snapshot_id,
+            "DBClusterIdentifier": db,
+            "DBClusterSnapshotArn": None,
+            "SnapshotType": None,
+            "SnapshotCreateTime": None,
+            "AvailabilityZones": None,
+            "VpcId": None,
+        }
+    except botocore.exceptions.ParamValidationError as error:
+        raise ValueError(f"The provided parameters are incorrect: {error}")
+    except Exception as error:
+        raise Exception(f"Error creating snapshot for {db}: {error}")
+
+
+def create_db_backups(dbs: List[str]) -> List[Snapshot]:
+    backups = []
+    for db in dbs:
+        backup = create_snapshot(db)
+        backups.append(backup)
+    return backups
+
+
+if __name__ == "__main__":
+    # TODO: pass in db names through cli
+    dbs = ["kraken-aurora-cluster"]
+    backups = create_db_backups(dbs)
+    print(backups)
